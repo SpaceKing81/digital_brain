a334f8b1a900176d4a1020748dad8a20461e282f
Mohammed Vohra <mohammedvohra5@gmail.com>
2025-11-02 14:28:10 -0500

    Definitly overwrites the file

--- examples/download.rs
+++ examples/download.rs
@@ -4,8 +4,8 @@ use digital_brain::Spirion;
 
 fn main() {
   let (mut brain,inputs, outputs) = Spirion::spin_up_new(
-    Some(1000), 
-    40, 40,
+    Some(10), 
+    4, 4,
     false,
   );
   brain.save_as_bin();
