685280a5bd48eebf66b8d4ac4bfce06a4bc9b42d
Mohammed Vohra <mohammedvohra5@gmail.com>
2025-11-02 14:27:15 -0500

    testing to see if it overwrites

--- examples/download.rs
+++ examples/download.rs
@@ -4,8 +4,8 @@ use digital_brain::Spirion;
 
 fn main() {
   let (mut brain,inputs, outputs) = Spirion::spin_up_new(
-    Some(10), 
-    2 as u128, 2,
+    Some(1000), 
+    40, 40,
     false,
   );
   brain.save_as_bin();
