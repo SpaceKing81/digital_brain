3dba87c1fa01a5e831c02cd8eaf72c235b1ba960
Mohammed Vohra <mohammedvohra5@gmail.com>
2026-02-05 20:06:47 -0500

    working on building snake. not ready yet

--- examples/normal_snake.rs
+++ examples/normal_snake.rs
@@ -83,14 +83,14 @@ struct Matrix<T> {
 impl<T> Matrix<T> {
 /*
 matrix
-  0|1|2
-  3|4|5
-  6|7|8
+  0|1|2 row0
+  3|4|5 row1
+  6|7|8 row2
   ->
   data
   [0,1,2,3,4,5,6,7,8]
 */
-  /// Makes a new 0 index matrix. (0,0) is a11
+  /// Makes a new 0 index matrix. (0,0) is a11, (1,2) is a23
   pub fn new(rows:usize, default: T) -> Self where T:Clone,{
     Matrix {
       data: vec![default; rows*rows],
@@ -113,29 +113,39 @@ matrix
   }
 
 }
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Copy)]
+#[derive(Clone, Debug, PartialEq, Copy)]
 struct Coords {
   row:usize,
   col:usize,
 }
+impl Coords {
+  fn add(&mut self, (row,col):(usize,usize)) {
+    self.row += row;
+    self.col += col;
+  }
+  fn sub(&mut self, (row,col):(usize,usize)) {
+    self.row.saturating_sub(row);
+    self.col.saturating_sub(col);
+  }
+}
+
 struct SnakeGame {
-  current_frame:Matrix<bool>,
+  current_frame:Matrix<f32>,
+  apple_gradient:Matrix<f32>,
   snake:Snake,
+  apple:Coords,
   
   score:usize,
   pixle_size:f32,
 }
 struct Snake {
-  head:(usize,usize),
+  head:Coords,
   dir:Dir,
-  path:Vec<(usize,usize)>,
+  path:Vec<Coords>,
   length:u32,
 }
 
-struct Apple {
-  row:usize,
-  col:usize,
-}
+
 
 #[derive(Clone, Copy)]
 enum Dir {
@@ -143,11 +153,12 @@ enum Dir {
   Right,
   Up,
   Down,
+  None,
 }
 
 
 impl Snake {
-  fn new(center:Vec2, level:f32) -> Self {
+  fn new(center:Coords, level:f32) -> Self {
     // let x = rand::gen_range(-1.0, 1.0);
     // let dir = if x.is_sign_positive() {
     //   Dir::Up
@@ -167,77 +178,37 @@ impl Snake {
     self.path.remove(0);
     self.path.push(self.head);
     self.head = match dir {
-      Dir::Down => {self.head + Vec2::new(0.0, -1.0)}
-      Dir::Left =>{self.head + Vec2::new(-1.0, 0.0)}
-      Dir::Right =>{self.head + Vec2::new(1.0, 0.0)}
-      Dir::Up =>{self.head + Vec2::new(0.0, 1.0)}
+      Dir::Down => {self.head.add(1,0)}
+      Dir::Right =>{self.head.add(0,1);}
+      Dir::Left =>{self.head.sub(0,1);}
+      Dir::Up =>{self.head.sub(1,0);}
+      _=> panic!("Cannot have the head not have a direction")
     };
   }
+  fn ate(&mut self) {
+    self.path.push(self.head);
+    self.length += 1;
+  }
   fn check_edge_collide(&mut self) {
+    /*
+    if collision, restart the game
+     */
     todo!()
   }
   fn check_self_collide(&mut self) {
-    todo!()
-  }
-}
-
-impl Apple {
-  fn new() -> Self {
-    todo!()
-  }
-  fn change_pos(&mut self, snake:&Snake) {
+    /*
+    restart game if colliding.
+     */
     todo!()
   }
 }
 
 impl SnakeGame {
   fn new(game_size:Option<usize>, level:f32) -> Self {
-    
-    let (row, col) = new.get_ball_pos();
-    new.current_frame.set(row, col, true).unwrap_or_default();
-    new.current_frame.set(0,0, true).unwrap_or_default();
-    new.current_frame.set(0,1, true).unwrap_or_default();
-    new;
     todo!();
   }
   fn progress_frame(&mut self)  {
-    self.move_paddle(get_move());
-    let (row,col) = self.get_ball_pos();
-    if let Some(_) = self.current_frame.get(
-      row,
-      col,
-    ) {
-    self.current_frame.set(
-      row,
-      col,
-      false,
-    ).unwrap_or_default();
-  }
-
-
-    if self.ball_hit_paddle(row, col) {
-      self.ball.bounce_left_right(); 
-      self.score +=1;
-    }
-    if (col + 1) == self.current_frame.cols || col == 0 {
-      self.ball.bounce_top_bottom();
-    }
-    if (row + 1) == self.current_frame.rows || row == 0 {
-      self.ball.bounce_left_right();
-    }
-    
-    self.ball.forward();
-    let (row,col) = self.get_ball_pos();
-    if let Some(_) = self.current_frame.get(
-      row,
-      col,
-    ) {
-    self.current_frame.set(
-      row,
-      col,
-      true,
-    ).unwrap_or_default();
-  }
+    todo!()
   }
   fn move_paddle(&mut self, direction:Move) {
     if let Some(_) = self.current_frame.get(
@@ -337,38 +308,51 @@ impl SnakeGame {
         true
       ).unwrap_or_default();
     }
+    todo!()
   }
+  
   fn draw(&self) {
     let length = self.pixle_size;
     for xcell in 0..self.current_frame.rows {
       for ycell in 0..self.current_frame.cols {
-        if let Some(state) = self.current_frame.get(xcell, ycell) {
-          let color = if *state {WHITE} else {BLACK};
+        if let Some(value) = self.current_frame.get(xcell, ycell) {
+          let color = if *state > 0 {
+            WHITE
+          } else {
+            // needs a color fn that makes a color red->black based on the apple gradient
+            todo!()
+          };
           draw_rectangle((xcell as f32) * length, (ycell as f32) * length, length, length, color);
         }}}
   }
- 
-  fn get_ball_pos(&self) -> (usize,usize) {
-    let ballx = self.ball.pos.x;
-    let bally = self.ball.pos.y;
-    (self.pixle_to_grid(ballx), self.pixle_to_grid(bally))
-    // x->col, y->row
+
+  fn snake_ate_apple(&mut self) -> bool {
+    /*
+    1) make the snake longer by adding the current head pos to the vec wo removing one
+    2) change apple pos
+    3) increase snake length value
+     */
+    todo!()
   }
-  fn ball_hit_paddle(&self, row:usize, col:usize) -> bool {
-    if row != 1 {return false;}
-    if let Some(&check) = self.current_frame.get(0, col) {
-      return check;
-    } 
-    panic!("Ball left grid");
+  fn change_apple_pos(&mut self) {
+    /*
+    take the current positions of the snake body, and place the apple somewhere random in the extra space
+     */
+    todo!()
   }
+  fn check_snake_ate(&self) -> bool {
+    self.apple == self.snake.head
+  }
+
+
   fn pixle_to_grid(&self, pixle:f32) -> usize {
     (pixle/self.pixle_size).floor() as usize
   }
   fn level_up(&mut self) {
-    self.score = 0;
-    self.ball.vel += 1.0;
+    todo!()
   }
 }
+
 fn pixle_size_calculator(game_size:usize) -> f32 {
   if game_size == 0 {panic!("Chosen game size is too large");}
   let smallest = std::cmp::min(screen_height().round() as i32, screen_width().round() as i32);
@@ -376,16 +360,21 @@ fn pixle_size_calculator(game_size:usize) -> f32 {
     return (smallest as f32/game_size as f32) as f32
   } else { panic!("Chosen game size is too large") }
 }
-fn get_move() -> Move {
+fn get_move() -> Dir {
   if is_key_released(KeyCode::Down) {
-    return Move::Down;
+    return Dir::Down;
   }
   if is_key_released(KeyCode::Up) {
-    return Move::Up;
+    return Dir::Up;
   }
-  
-  
-  Move::None
+  if is_key_released(KeyCode::Left) {
+    return Dir::Left;
+  }
+  if is_key_released(KeyCode::Right) {
+    return Dir::Right;
+  }
+
+  Dir::None
 }
 
 
